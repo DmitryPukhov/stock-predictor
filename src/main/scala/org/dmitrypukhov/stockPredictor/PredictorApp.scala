af31a47ba83ff099a9041d036b134ae5995273ec
Dmitry <dmitry@dmitrypukhov.pro>
2017-07-22 01:11:30 +0300

    Updated to use spark streaming.

--- src/main/scala/org/dmitrypukhov/stockPredictor/PredictorApp.scala
+++ src/main/scala/org/dmitrypukhov/stockPredictor/PredictorApp.scala
@@ -1,19 +1,33 @@
 package org.dmitrypukhov.stockPredictor
 
 
+import org.apache.spark
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 /**
   * Application entry with main()
   */
 object PredictorApp extends App {
 
-  println("Test")
-  val spark = SparkSession.builder.master("local").appName("Predictor").getOrCreate()
-  import spark.implicits._
-  val ds = spark.createDataset(List("aaa","bbb","ccc"))
-  println(ds.collect.mkString(","))
-  spark.stop()
+  println("Predictor")
+
+  // Create streaming context
+  val conf = new SparkConf().setMaster("local").setAppName("Predictor")
+  val ssc = new StreamingContext(conf, Seconds(1))
+
+  // Create stream
+  val inputDir = "data"
+  val ds = ssc.textFileStream(inputDir)
+
+  // Print data for test
+  ds.print(10)
+
+  // Start the app
+  ssc.start()
+  ssc.awaitTermination()
+  ssc.stop()
 
 
 }
